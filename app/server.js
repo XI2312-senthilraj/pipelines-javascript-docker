10a399085d42c10d63b5dad07b5599f719ff701b
XI2312-senthilraj <82937378+XI2312-senthilraj@users.noreply.github.com>
2022-09-21 05:21:27 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Red Eye, Progressive release is awesome!')
 })
 
 app.listen(port, () => {
